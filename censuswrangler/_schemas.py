6fcf5eae96bfdf1102a25153c5e55dc65db8b7af
Kyle Ross <107285758+Kyle-Ross@users.noreply.github.com>
2025-03-16 17:11:19 +1100

    Broader name and defined metadata schema

---
+++ censuswrangler/_schemas.py
@@ -0,0 +1,24 @@
+"""Defining pandera schemas which will be used for validation of things like the config file."""
+
+import pandera as pa
+
+config_schema = pa.DataFrameSchema(
+    {
+        "DATA_FILE_CODE": pa.Column(str),
+        "FIELD_SHORT": pa.Column(str),
+        "FIELD_LONG": pa.Column(str),
+        "VALUE_DESC": pa.Column(str),
+        "GROUP": pa.Column(str),
+    }
+)
+
+metadata_schema = pa.DataFrameSchema(
+    {
+        "sequential": pa.Column(str),
+        "short": pa.Column(str),
+        "long": pa.Column(str),
+        "datapackfile": pa.Column(str),
+        "profiletable": pa.Column(str),
+        "columnheadingdescriptionprofile": pa.Column(str),
+    }
+)
