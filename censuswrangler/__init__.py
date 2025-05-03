27f5744ff6f0c7dbe221369fa7641846f6452a24
Kyle Ross <107285758+Kyle-Ross@users.noreply.github.com>
2025-05-03 16:19:49 +1000

    Fixes to __init__.py

--- censuswrangler/__init__.py
+++ censuswrangler/__init__.py
@@ -1,7 +1,7 @@
 """
 censuswrangler package.
 
-This package provides tools for extracting census data from the Australian Bureau of Statistics (ABS) datapack structure. 
+This package provides tools for extracting census data from the Australian Bureau of Statistics (ABS) datapack structure.
 It includes utilities for managing datapack outputs using validated configuration templates.
 
 Interface:
@@ -11,5 +11,9 @@ Interface:
 
 from censuswrangler.config import create_config_template
 from censuswrangler.census import Census
+from censuswrangler import config  # noqa: F401
+from . import _schemas  # noqa: F401
+from . import _data  # noqa: F401
+from . import _pack  # noqa: F401
 
 __all__ = ["create_config_template", "Census"]
